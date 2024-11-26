6ab78a3cbb8bc91af6cbb0c557ed834622c4235e
Daniel Orio <danielislas3@live.com>
2024-11-26 11:28:32 -0600

    refactor: restructure app.vue and update nuxt.config.ts for improved layout and compatibility

---
+++ app/app.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <NuxtRouteAnnouncer />
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
+  </div>
+</template>
+
+<script setup>
+
+  const appName = 'OKIL'
+
+  const route = useRoute()
+  useHead({
+    titleTemplate: pageTitle => (pageTitle ? `${pageTitle} | ${appName}` : appName),
+    title: computed(() => route.meta.title || '')
+  })
+</script>
