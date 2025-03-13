4fd844dbfcaf013fc39b484c17b19530e785efb2
Daniel Orio <danielislas3@live.com>
2025-03-13 15:02:49 -0600

    🧹 lint: update ESLint configuration and dependencies for improved code quality

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,27 +1,27 @@
-import withNuxt from './.nuxt/eslint.config.mjs'
-import eslintConfigPrettier from 'eslint-config-prettier'
-import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
+import withNuxt from "./.nuxt/eslint.config.mjs";
+import eslintConfigPrettier from "eslint-config-prettier";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
 
 export default withNuxt(
   eslintConfigPrettier,
   eslintPluginPrettierRecommended,
 
   {
-    name: 'custom-config',
+    name: "custom-config",
 
-    files: ['**/*.ts', '**/*.vue'],
+    files: ["**/*.ts", "**/*.vue"],
 
     rules: {
-      'no-debugger': 'error',
-      'semi': ['error', 'never'],
-      'no-console': 'warn',
-      'no-unused-expressions': 'off',
-      '@typescript-eslint/no-explicit-any': 'off',
-      '@typescript-eslint/ban-ts-ignore': 'off',
-      '@typescript-eslint/no-empty-object-type': 'warn',
-      '@typescript-eslint/no-unused-expressions': 'off',
-
-      'vue/require-default-prop': 'off'
-    }
-  }
-)
+      "no-debugger": "error",
+      semi: ["error", "never"],
+      "no-console": "warn",
+      "no-unused-expressions": "off",
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/ban-ts-ignore": "off",
+      "@typescript-eslint/no-empty-object-type": "warn",
+      "@typescript-eslint/no-unused-expressions": "off",
+      "@typescript-eslint/no-empty-object-type": "warn",
+      "vue/require-default-prop": "off",
+    },
+  },
+);
